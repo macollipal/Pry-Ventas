4cc37f61b8d7a998dfed2562e9effd297194a695
macollipal <marcelocollipal@gmail.com>
2021-08-09 19:29:33 -0400

    app funcionando con minimo requerido(pagina, base datos, urls)

--- app/app/urls.py
+++ app/app/urls.py
@@ -1,3 +1,4 @@
+  
 """app URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -13,9 +14,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+
+from django import urls
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+
+    path('',include(('bases.urls', 'bases'), namespace='bases')),
+    #path('inv/',include(('inv.urls', 'inv'), namespace='inv')),
+    #path('cmp/',include(('cmp.urls', 'cmp'), namespace='cmp')),
+    
     path('admin/', admin.site.urls),
+
 ]
--- app/bases/models.py
+++ app/bases/models.py
@@ -1,3 +1,16 @@
+from abc import abstractmethod
 from django.db import models
+
+from django.contrib.auth.models import User
+
+class ClaseModelo(models.Model):
+    estado = models.BooleanField(default=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    modified_by = models.IntegerField(blank=True, null=True)
+    
+    class Meta:
+        abstract =True
 
 # Create your models here.
